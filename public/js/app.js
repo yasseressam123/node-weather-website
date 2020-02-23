2f1828cf8bf5a466bca1f9bc5748f57d6615152c
yasser mohamed <yassermohammed42781@gmail.com>
2020-02-23 15:41:00 +0200

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location = search.value
     mesg1.textContent = 'Loading...'
     mesg2.textContent = ''
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             mesg1.textContent= data.error
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDictoryPath = path.join(__dirname,'../public')
@@ -81,6 +82,6 @@ app.get('*',(req,res)=>{
         errorMessage:'Page not found.'
     })
 })
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('Server is up on port ' + port)
 })
